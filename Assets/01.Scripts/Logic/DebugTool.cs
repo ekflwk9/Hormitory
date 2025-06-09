7fa5320b44b2542b6d067b69dc6ab89f48701906
ekflwk9 <ekflwk9@naver.com>
2025-06-09 12:09:41 +0900

    프로젝트 초기 셋팅

---
+++ Assets/01.Scripts/Logic/DebugTool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DebugTool : EditorWindow
+{
+    [MenuItem("Window/DebugTool")]
+    public static void ShowWindow()
+    {
+        GetWindow<DebugTool>("DebugTool");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("어드레서블 로드")) Addressable();
+    }
+
+    private void Addressable()
+    {
+
+    }
+}
