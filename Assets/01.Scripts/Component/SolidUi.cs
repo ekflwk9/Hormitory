463fe585b182be43b853c8265bebac525e5c282f
ekflwk9 <ekflwk9@naver.com>
2025-06-13 15:36:17 +0900

    [feat] 병합

---
+++ Assets/01.Scripts/Component/SolidUi.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SolidUi : MonoBehaviour
+{
+    [Header("고정 크기 값")]
+    [SerializeField] private float scale;
+    private Transform target;
+
+    private void Start()
+    {
+        //target = ;
+    }
+
+    private void LateUpdate()
+    {
+        var distance = Vector3.Distance(transform.position, target.position);
+        this.transform.localScale = Vector3.one * (distance * scale);
+
+        this.transform.forward = target.transform.forward;
+    }
+}
