e20f430fbe7ad300c0bad5f9926904d973ecb50f
pjhzzazz <pjhzzazzz@naver.com>
2025-06-16 02:56:55 +0900

    [Feat] 기존 Knife 에셋 제거 및 에셋 추가 콜라이더 처리 개선

--- Assets/01.Scripts/Player/Player_Battle/WeaponKnifeCollider.cs
+++ Assets/01.Scripts/Player/Player_Battle/WeaponKnifeCollider.cs
@@ -25,7 +25,7 @@ public class WeaponKnifeCollider : MonoBehaviour
         this.damage = damage;
         collider.enabled = true;
 
-        StartCoroutine("DisableByTime", 0.1f);
+        StartCoroutine("DisableByTime", 0.2f);
     }
 
     private IEnumerator DisableByTime(float time)
