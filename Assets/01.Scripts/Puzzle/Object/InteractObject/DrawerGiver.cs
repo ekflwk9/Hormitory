480ee67700396679fe5e0220a4c438831663c773
Hicanos <ruda32168@gmail.com>
2025-06-18 06:45:17 +0900

    [Feat] Puzzle 및 상호작용 아이템 배치
    퍼즐 및 상호작용 아이템 배치 완료

--- Assets/01.Scripts/Puzzle/Object/InteractObject/DrawerGiver.cs
+++ Assets/01.Scripts/Puzzle/Object/InteractObject/DrawerGiver.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class DrawerGiver : ItemReceiver,IInteractable
 {
     [SerializeField] private ItemSO giveItem; // 상호작용 시 주어질 아이템
+    private bool isGetItem = false; // 아이템 획득 여부
 
     public override void InteractAction()
     {
-        if(isCleard)
+        if(isCleard && isGetItem == false)
         {
             ItemManager.instance.RegisterItem(giveItem, giveItem.ItemID); // 아이템 획득
             SoundManager.PlaySfx(SoundCategory.Interaction, "OpenDoor");
+            UiManager.Instance.Get<TalkUi>().Popup("서랍에서 작은 열쇠를 발견했다.");
             UiManager.Instance.Get<MissionUi>().Popup("목표 : 열쇠의 사용처 찾기");
+            isGetItem = true; // 아이템 획득 상태 변경
         }
         else
         {
--- Assets/01.Scripts/Puzzle/Object/InteractObject/Trunk.cs
+++ Assets/01.Scripts/Puzzle/Object/InteractObject/Trunk.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Trunk : ItemReceiver, IInteractable
 {
     [SerializeField] private ItemSO giveItem; // 퍼즐 완료 후 주는 아이템
+    private bool isGetItem = false; // 아이템 획득 여부
     public override void InteractAction()
     {
-        if (isCleard)
+        if (isCleard && isGetItem==false)
         {
             UiManager.Instance.Get<TalkUi>().Popup("안에 무슨 열쇠가 있군...");
             SoundManager.PlaySfx(SoundCategory.Interaction, "OpenDoor");
             ItemManager.instance.RegisterItem(giveItem, giveItem.ItemID);
 
             UiManager.Instance.Get<MissionUi>().Popup("목표 : 열쇠의 사용처 찾기");
+            isGetItem = true; // 아이템 획득 상태 변경
         }
         else
         {            
